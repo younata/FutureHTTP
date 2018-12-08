2eee2c4b338115d4731a3d67c57a42e742507330
Rachel Brindle <you@subluminal.net>
2018-12-07 21:25:53 -0800

    Update for swift 4 tooling

--- Package.swift
+++ Package.swift
@@ -1,9 +1,20 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "FutureHTTP",
+    products: [
+        .library(name: "FutureHTTP", targets: ["FutureHTTP"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3),
-        .Package(url: "https://github.com/cbguder/CBGPromise.git", majorVersion: 0, minor: 4)
+        .package(url: "https://github.com/antitypical/Result.git", .upToNextMinor(from: "4.0.0")),
+        .package(url: "https://github.com/cbguder/CBGPromise.git", .upToNextMinor(from: "0.5.0")),
+
+        .package(url: "https://github.com/quick/Quick.git", .upToNextMinor(from: "1.3.2")),
+        .package(url: "https://github.com/quick/Nimble.git", .upToNextMinor(from: "7.3.1"))
+    ],
+    targets: [
+        .target(name: "FutureHTTP", dependencies: ["Result", "CBGPromise"], path: "Sources"),
+        .testTarget(name: "FutureHTTPTests", dependencies: ["FutureHTTP", "Quick", "Nimble"])
     ]
 )
